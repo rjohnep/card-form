3ea625fa0c71e6ca988d9b68b858c5173be39467
Eugene Rogach <eugene.rogach@pandadoc.com>
2019-10-27 21:03:33 +0100

    base form, walidation and masks wip

--- app/containers/CardForm/components/Form/index.tsx
+++ app/containers/CardForm/components/Form/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { Wrapper, Field } from './styled';
+import { Wrapper, Field, Row } from './styled';
 
 export const Form: FC = () => (
   <Wrapper>
@@ -13,5 +13,37 @@ export const Form: FC = () => (
         autoComplete="off"
       />
     </Field>
+
+    <Field>
+      <label htmlFor="cardNumber">Card Holder</label>
+      <input
+        type="text"
+        id="cardHolder"
+        data-ref="cardName"
+        autoComplete="off"
+      />
+    </Field>
+
+    <Row>
+      <Field group>
+        <label htmlFor="cardNumber">Expiration Date</label>
+        <select id="cardMonth" data-ref="cardDate" defaultValue="Month">
+          <option disabled>Month</option>
+          <option>1</option>
+          <option>2</option>
+        </select>
+
+        <select id="cardYear" data-ref="cardDate" defaultValue="Year">
+          <option disabled>Year</option>
+          <option>2019</option>
+          <option>2020</option>
+        </select>
+      </Field>
+
+      <Field short>
+        <label>CVV</label>
+        <input type="text" id="cardCvv" maxLength={4} autoComplete="off" />
+      </Field>
+    </Row>
   </Wrapper>
 );
--- app/containers/CardForm/components/Form/styled/index.ts
+++ app/containers/CardForm/components/Form/styled/index.ts
@@ -1,4 +1,5 @@
 import { styled } from '@app/theme';
+import { css, FlattenSimpleInterpolation } from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -7,19 +8,44 @@ export const Wrapper = styled.div`
   width: 100%;
   max-width: 600px;
   margin-top: -125px;
-  padding: 150px 25px 25px;
+  padding: 170px 25px 25px;
 
   background: #add8e645;
   border-radius: 15px;
   box-shadow: 0 25px 50px 0 rgba(14, 42, 90, 0.55);
 `;
 
-export const Field = styled.div`
+const groupedStyle = css`
+  flex-wrap: wrap;
+  flex-direction: row;
+
+  label {
+    width: 100%;
+  }
+
+  input,
+  select {
+    margin-right: 20px;
+    min-width: 100px;
+  }
+`;
+
+const shortStyle = css`
+  max-width: 150px;
+`;
+
+export const Field = styled.div<{ group?: boolean; short?: boolean }>`
   width: 100%;
   display: flex;
   flex-direction: column;
+  margin-bottom: 20px;
 
-  input {
+  label {
+    font-size: 12px;
+  }
+
+  input,
+  select {
     height: 40px;
     padding: 10px;
 
@@ -27,4 +53,15 @@ export const Field = styled.div`
     border: 1px solid #ccc;
     border-radius: 5px;
   }
+
+  ${({ group }): undefined | false | FlattenSimpleInterpolation =>
+    group && groupedStyle}
+
+  ${({ short }): undefined | false | FlattenSimpleInterpolation =>
+    short && shortStyle}
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
