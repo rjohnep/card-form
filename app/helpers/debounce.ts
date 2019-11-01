6c2f7821bb4f2803a1bdd0192d290a8443055672
Eugene Rogach <eugene.rogach@pandadoc.com>
2019-11-01 16:50:11 +0100

    removed debounce

--- app/helpers/debounce.ts
+++
@@ -1,7 +0,0 @@
-export const debounce = (fn, delay) => {
-  let timeoutId;
-  return function(...args) {
-    clearInterval(timeoutId);
-    timeoutId = setTimeout(() => fn.apply(this, args), delay);
-  };
-};
